76d1ba27ccbe7f2e4ee2adb769a60c15203b50d3
xinru <s3a632065@student.ncut.edu.tw>
2020-01-05 01:52:59 +0800

    宣告屬性能被批量賦值
    修改資料態time為可空值

--- app/Item.php
+++ app/Item.php
@@ -6,6 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class Item extends Model
 {
+    // 這些屬性能被批量賦值
+    public $fillable = [ 'id', 'order_id', 'product_id', 'count' ];
+
     // 取得擁有此訂餐明細的訂餐。
     public function orders()
     {
--- app/Table.php
+++ app/Table.php
@@ -6,6 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class Table extends Model
 {
+    // 這些屬性能被批量賦值
+    public $fillable = [ 'id', 'count' ];
+
     //取得擁有此桌位的訂位明細。
     public function detail()
     {
