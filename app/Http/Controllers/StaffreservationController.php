8227e36ef5ffade91c7fde693e491dae335aa0d3
yingjhen <jhenlin0220@gmail.com>
2020-01-05 19:38:43 +0800

    員工訂位頁面(index)，顯示資料

---
+++ app/Http/Controllers/StaffreservationController.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Reservation;
+
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+
+use App\Http\Requests\PostRequest;
+
+class StaffreservationController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+        $reservations=Reservation::orderBy('id','ASC')->get();
+        $data=['reservations'=>$reservations];
+        return View('staff.reservation.index',$data);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+        
+    }
+
+
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        Reservation::create($request->all());
+        return redirect()->route('staff.reservation.index');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Users  $users
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Reservation $reservations)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Users  $users
+     * @return \Illuminate\Http\Response
+     */
+    public function edit()
+    {
+
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Users  $users
+     * @return \Illuminate\Http\Response
+     */
+    public function update()
+    {
+
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Users  $users
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy()
+    {
+
+    }
+}
