dd0262a654ad2e071815988b24b47157e7679003
yingjhen <jhenlin0220@gmail.com>
2020-01-05 15:03:21 +0800

    新增後台產品查詢頁面

---
+++ resources/views/staff/dashboard/index.blade.php
@@ -0,0 +1,50 @@
+@extends('admin.layouts.master')
+
+@section('title', '後台管理')
+
+@section('content')
+<!-- Page Heading -->
+<div class="row">
+    <div class="col-lg-12">
+        <h1 class="page-header">
+            後台管理 <small></small>
+        </h1>
+        <ol class="breadcrumb">
+            <li class="active">
+                <i class="fa fa-dashboard"></i> 後台管理
+                <i class="fa fa-dashboard"></i> <a href="{{route('admin.posts.index')}}">管理員管理</a>
+                <i class="fa fa-dashboard"></i> <a href="{{route('admin.product.index')}}">產品管理</a>
+                <i class="fa fa-dashboard"></i> <a href="{{route('admin.dashboard.search')}}">查詢</a>
+            </li>
+        </ol>
+
+    </div>
+</div>
+<!-- /.row -->
+
+<div class="row">
+    <p>&nbsp;</p>
+    <p>&nbsp;</p>
+    <p>&nbsp;</p>
+    <p>&nbsp;</p>
+    <p>&nbsp;</p>
+    <p>&nbsp;</p>
+    <p>&nbsp;</p>
+    <p>&nbsp;</p>
+    <p>&nbsp;</p>
+    <p>&nbsp;</p>
+    <p>&nbsp;</p>
+    <p>&nbsp;</p>
+    <p>&nbsp;</p>
+    <p>&nbsp;</p>
+    <p>&nbsp;</p>
+    <p>&nbsp;</p>
+    <p>&nbsp;</p>
+    <p>&nbsp;</p>
+    <p>&nbsp;</p>
+    <p>&nbsp;</p>
+    <p>&nbsp;</p>
+</div>
+<!-- /.row -->
+
+@endsection
