47e106b94f85664e1bff24d33402331c258eeb8b
xinru <s3a632065@student.ncut.edu.tw>
2020-01-07 14:02:45 +0800

    修改新增訂餐頁面

--- resources/views/member/myorder/create.blade.php
+++ resources/views/member/myorder/create.blade.php
@@ -52,11 +52,83 @@
                     <div class="input-group-prepend">
                         <span class="input-group-text">產品名稱：</span>
                     </div>
-                    <select name="id" class="form-control">
+                    <select name="product_id" class="form-control">
                         @foreach($products as $product)
                             @for($n=1;$n<=count($product);$n++)
                                 @if($product->whether == 1)
-                                    <option value="n">{{$product->name}} ${{$product->price}}</option>
+                                    @if($product->id == 1)
+                                        <option value='1'>{{$product->name}} ${{$product->price}}</option>
+                                    @elseif($product->id == 2)
+                                        <option value='2'>{{$product->name}} ${{$product->price}}</option>
+                                    @elseif($product->id == 3)
+                                        <option value='3'>{{$product->name}} ${{$product->price}}</option>
+                                    @elseif($product->id == 4)
+                                        <option value='4'>{{$product->name}} ${{$product->price}}</option>
+                                    @elseif($product->id == 5)
+                                        <option value='5'>{{$product->name}} ${{$product->price}}</option>
+                                    @elseif($product->id == 6)
+                                        <option value='6'>{{$product->name}} ${{$product->price}}</option>
+                                    @elseif($product->id == 7)
+                                        <option value='7'>{{$product->name}} ${{$product->price}}</option>
+                                    @elseif($product->id == 8)
+                                        <option value='8'>{{$product->name}} ${{$product->price}}</option>
+                                    @elseif($product->id == 9)
+                                        <option value='9'>{{$product->name}} ${{$product->price}}</option>
+                                    @elseif($product->id == 10)
+                                        <option value='10'>{{$product->name}} ${{$product->price}}</option>
+                                    @elseif($product->id == 11)
+                                        <option value='11'>{{$product->name}} ${{$product->price}}</option>
+                                    @elseif($product->id == 12)
+                                        <option value='12'>{{$product->name}} ${{$product->price}}</option>
+                                    @elseif($product->id == 13)
+                                        <option value='13'>{{$product->name}} ${{$product->price}}</option>
+                                    @elseif($product->id == 14)
+                                        <option value='14'>{{$product->name}} ${{$product->price}}</option>
+                                    @elseif($product->id == 15)
+                                        <option value='15'>{{$product->name}} ${{$product->price}}</option>
+                                    @elseif($product->id == 16)
+                                        <option value='16'>{{$product->name}} ${{$product->price}}</option>
+                                    @elseif($product->id == 17)
+                                        <option value='17'>{{$product->name}} ${{$product->price}}</option>
+                                    @elseif($product->id == 18)
+                                        <option value='18'>{{$product->name}} ${{$product->price}}</option>
+                                    @elseif($product->id == 19)
+                                        <option value='19'>{{$product->name}} ${{$product->price}}</option>
+                                    @elseif($product->id == 20)
+                                        <option value='20'>{{$product->name}} ${{$product->price}}</option>
+                                    @elseif($product->id == 21)
+                                        <option value='21'>{{$product->name}} ${{$product->price}}</option>
+                                    @elseif($product->id == 22)
+                                        <option value='22'>{{$product->name}} ${{$product->price}}</option>
+                                    @elseif($product->id == 23)
+                                        <option value='23'>{{$product->name}} ${{$product->price}}</option>
+                                    @elseif($product->id == 24)
+                                        <option value='24'>{{$product->name}} ${{$product->price}}</option>
+                                    @elseif($product->id == 25)
+                                        <option value='25'>{{$product->name}} ${{$product->price}}</option>
+                                    @elseif($product->id == 26)
+                                        <option value='26'>{{$product->name}} ${{$product->price}}</option>
+                                    @elseif($product->id == 27)
+                                        <option value='27'>{{$product->name}} ${{$product->price}}</option>
+                                    @elseif($product->id == 28)
+                                        <option value='28'>{{$product->name}} ${{$product->price}}</option>
+                                    @elseif($product->id == 29)
+                                        <option value='29'>{{$product->name}} ${{$product->price}}</option>
+                                    @elseif($product->id == 30)
+                                        <option value='30'>{{$product->name}} ${{$product->price}}</option>
+                                    @elseif($product->id == 31)
+                                        <option value='31'>{{$product->name}} ${{$product->price}}</option>
+                                    @elseif($product->id == 32)
+                                        <option value='32'>{{$product->name}} ${{$product->price}}</option>
+                                    @elseif($product->id == 33)
+                                        <option value='33'>{{$product->name}} ${{$product->price}}</option>
+                                    @elseif($product->id == 34)
+                                        <option value='34'>{{$product->name}} ${{$product->price}}</option>
+                                    @elseif($product->id == 35)
+                                        <option value='35'>{{$product->name}} ${{$product->price}}</option>
+                                    @else
+                                        <option value='36'>{{$product->name}} ${{$product->price}}</option>
+                                    @endif
                                 @endif
                             @endfor
                         @endforeach
