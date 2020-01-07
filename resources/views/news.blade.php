039a69fa4708d3982fb25a1e610b684f572e6d62
xinru <s3a632065@student.ncut.edu.tw>
2020-01-08 03:06:31 +0800

    新增照片

---
+++ resources/views/news.blade.php
@@ -0,0 +1,122 @@
+@extends('layouts.master')
+
+@section('title', '活動公告')
+
+@section('content')
+
+    <!-- Header -->
+    <header class="masthead" style="background-image: url('{{asset('img/15754412019827956001905518475630.jpg')}}')">
+        <div class="container d-flex h-100 align-items-center">
+            <div class="mx-auto text-center" >
+                <h3 class="display-1 mx-auto my-0 text-white" style="font-family: 微軟正黑體"> 公告事項 </h3>
+            </div>
+        </div>
+    </header>
+
+    <section id="news" class="about-section text-left carousel slide" data-ride="carousel" style="background: #290e05">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-1 " style="background: #290e05">
+                </div>
+                <div class="col-lg-10 mx-auto mb-5" style="font-family: 標楷體">
+                    <h3 class="text-white mb-5" style="color: #5ce7f4">2012/01/07 茶與生活</h3>
+                    <img class="img-fluid mb-3 mb-lg-0 float-right" src="{{asset("img/News20125154.jpg")}}" alt="">
+                </div>
+                <div class="col-lg-1 " style="background: #290e05">
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-lg-1 " style="background: #290e05">
+                </div>
+                <div class="col-lg-10 mx-auto mb-5" style="font-family: 標楷體">
+                    <form style="font-size: large ">
+                        <h5 class="mb-3" style="color: #00B8B8" >
+                            輕鬆泡好茶｜免費講座
+                        </h5>
+                        <h6 class="mb-3" style="color: #fffadc" >
+                            茶文化已源遠流長千多年歷史，帶領大家認識六大茶類、
+                            生活茶具介紹、市售茶包辨別、選購茶葉技巧，讓大家簡單泡好茶，
+                            輕鬆享受美好品茗時光。
+                        </h6>
+                        <h5 class="mb-3" style="color: #bdf491" >報名須知</h5>
+                        <h6 style="color: #ffffff" >課程報名不限性別，採預約報名制，如人數額滿則登記候補。</h6>
+                        <h6 style="color: #ffffff" >課程前一日會以簡訊提醒，欲取消報名請來電辦理，以維護候補民眾之權益。</h6>
+                        <h6 style="color: #ffffff" >請依照報名方式報名，Facebook活動頁面上按「讚」、「參加」或「有興趣」僅為活動調查意願參考，非屬完成報名。</h6>
+                        <h6 style="color: #ffffff" >尊重講師智慧財產權，課程全程禁止錄音、錄影，懇請配合。</h6>
+                        <h6 style="color: #ffffff" >活動地點：花蓮市建中路11號</h6>
+                        <h6 style="color: #ffffff" >課程報名不限性別，採預約報名制，如人數額滿則登記候補。</h6>
+                        <h6 style="color: #ffffff" >電話報名➔ 07-3926711</h6>
+                        <h6 style="color: #ffffff" class="mb-5">服務時間：週二至週日上午8:30-12:00/下午1:30-5:30 (週一及國定假公休)</h6>
+                    </form>
+                </div>
+                <div class="col-lg-1 " style="background: #290e05">
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-lg-1 " style="background: #290e05">
+                </div>
+                <div class="col-lg-10 mx-auto mb-5" style="font-family: 標楷體">
+                    <h3 class="text-white mb-5" style="color: #5ce7f4">2012/01/02 春節時間公告</h3>
+                    <img class="img-fluid mb-3 mb-lg-0 float-right" src="{{asset("img/News127.jpg")}}" alt="">
+                </div>
+                <div class="col-lg-1 " style="background: #290e05">
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-lg-1 " style="background: #290e05">
+                </div>
+                <div class="col-lg-10 mx-auto mb-5" style="font-family: 標楷體">
+                    <form style="font-size: large ">
+                        <h5 class="mb-3" style="color: #00B8B8" >
+                            2020年 調整咖啡豆訂購出貨及收單日 通知
+                        </h5>
+                        <h6 style="color: #ffffff" >因應春節連假，半磅裝咖啡豆預購調整出貨日及收單日</h6>
+                        <h6 style="color: #ffffff" >咖啡豆線上訂購收單日：1/25(五) 18:00止</h6>
+                        <h6 style="color: #ffffff" >預計到貨日：2/2(六)前，貨到門市會由專人聯繫取貨。</h6>
+                        <h6 style="color: #ffffff" class="mb-5">2/11(一) 09:00 恢復正常訂購功能。</h6>
+                    </form>
+                </div>
+                <div class="col-lg-1 " style="background: #290e05">
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-lg-1 " style="background: #290e05">
+                </div>
+                <div class="col-lg-10 mx-auto mb-5" style="font-family: 標楷體">
+                    <h3 class="text-white mb-5" style="color: #5ce7f4">2019/12/23 聖誕好禮</h3>
+                    <img class="img-fluid mb-3 mb-lg-0 float-right" src="{{asset("img/News170.jpg")}}" alt="">
+                </div>
+                <div class="col-lg-1 " style="background: #290e05">
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-lg-1 " style="background: #290e05">
+                </div>
+                <div class="col-lg-10 mx-auto mb-5" style="font-family: 標楷體">
+                    <form style="font-size: large ">
+                        <h5 class="mb-3" style="color: #00B8B8" >
+                            聖誕好禮 | 消費滿千 現折$100
+                        </h5>
+                        <h6 style="color: #ffffff" class="mb-3">
+                            12/23-12/31，出示LINE@優惠券並持黑浮集團聯名儲值卡扣卡消費，結帳金額滿千現折100元
+                        </h6>
+                        <h5 style="color: #ff7d8e" class="mb-3">
+                            領取LINE@優惠券：
+                            <a href ="https://line.me/R/ti/p/%40revecafe">
+                            https://line.me/R/ti/p/%40revecafe</a>
+                        </h5>
+                        <h5 style="color: #bdf491" >注意事項：</h5>
+                        <h6 style="color: #ffffff" >(1) 此優惠活動不限新舊卡友,持儲值卡扣卡消費，結帳金額滿千享現折 100 元優惠。</h6>
+                        <h6 style="color: #ffffff" >(2) 消費不得累計,每筆消費僅提供乙次現折100 元之優惠,不得分次折抵。</h6>
+                        <h6 style="color: #ffffff" >(3) 為保障消費者權益,每組 LINE 帳號限使用一次優惠,兌換期限為 12/23-12/31,超過期限本 優惠自動失效,恕無法再行提供優惠。</h6>
+                        <h6 style="color: #ffffff" >(4) 本公司保留活動最終解釋之權利。</h6>
+
+                    </form>
+                </div>
+                <div class="col-lg-1 " style="background: #290e05">
+                </div>
+            </div>
+        </div>
+    </section>
+
+@endsection
